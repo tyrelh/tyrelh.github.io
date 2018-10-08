955de8a871cdd19e57a197013ddfac814335e8cd
Tyrel Hiebert <tyrelh@uvic.ca>
2018-10-08 15:42:02 -0700

    Add initial react-test

---
+++ react-test/precache-manifest.1a878893255677890527f14ab1b394e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a26eb0836dff595ec1d6",
+    "url": "/static/js/main.a26eb083.chunk.js"
+  },
+  {
+    "revision": "ace62ad78903889e8e0e",
+    "url": "/static/js/1.ace62ad7.chunk.js"
+  },
+  {
+    "revision": "a26eb0836dff595ec1d6",
+    "url": "/static/css/main.121db0cf.chunk.css"
+  },
+  {
+    "revision": "04c1bf8815960c084e7dd1447e1e4eec",
+    "url": "/index.html"
+  }
+];

c78251ec20eb331b89d631753a0ddae993e43324
tyrelh <tyrel.hiebert@gmail.com>
2022-04-09 20:33:57 -0700

    Updates

---
+++ images/posts/GrailsAssetMigration.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import MarkdownArticle from "../MarkdownArticle";
+
+export default class GrailsAssetMigration extends React.Component {
+  render() {
+    return <MarkdownArticle name="GrailsAssetMigration"/>;
+  }
+}

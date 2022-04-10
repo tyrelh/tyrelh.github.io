dfabf7dee3aee3d7ad1eae94e2155f9e5b3cb14b
tyrelh <tyrel.hiebert@gmail.com>
2022-04-09 20:29:06 -0700

    Updates

--- public/images/posts/GrailsAssetMigration.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import MarkdownArticle from "../MarkdownArticle";
-
-export default class GrailsAssetMigration extends React.Component {
-  render() {
-    return <MarkdownArticle name="GrailsAssetMigration"/>;
-  }
-}
--- utils/dateUtils.ts
+++
@@ -1,5 +0,0 @@
-import { PostData } from "../dtos/PostData"
-
-export const sortPostsByDate = (a: PostData, b: PostData): number => {
-    return ((new Date(b.date) as any) - (new Date(a.date) as any))
-}
